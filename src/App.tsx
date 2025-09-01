bea826b2a4bbbfee3238de934b3e49038c4d7c14
Sergio Hernández <sergiohernandez_1993@hotmail.com>
2025-09-01 11:20:05 +0200

    Creando formulario

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
+import { CriptoSearchForm } from "./components/CriptoSearchForm";
+
 
 
 export const App = () => {
+
+
+
   return (
-    <div>Cripto App</div>
-  )
-}
+    <div className="container">
+      <h1 className="app-title">
+        Cotizador de <span>Criptomonedas</span>
+      </h1>
+
+      <div className="content">
+        <CriptoSearchForm />
+      </div>
+    </div>
+  );
+};
