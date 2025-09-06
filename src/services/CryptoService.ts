550902cd05e2a62e0b3674120e15e34dd92e4883
Sergio Hernández <161878354+Sergiohc19@users.noreply.github.com>
2025-09-06 13:02:24 +0200

    actualizado CryptoService.ts para visualizar 20 cryptos

--- src/services/CryptoService.ts
+++ src/services/CryptoService.ts
@@ -1,26 +1,31 @@
-import axios from "axios"
-import { CryptoCurrenciesResponseSchema, CryptoPriceSchema } from "../schema/crypto-schema"
-import type { Pair } from "../types"
+import axios from "axios";
+import { CryptoCurrencyResponseSchema, CryptoPriceSchema } from "../schema/crypto-schema";
+import type { Pair } from "../types";
 
 export async function getCryptos() {
-    const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD"
-    const { data: { Data } } = await axios(url)
-    const result = CryptoCurrenciesResponseSchema.safeParse(Data)
-    console.log(Data)
-    console.log("result")
-    // console.log(result)
-    if (result.success) {
-        return result.data
-    }
+  const url = "https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD";
+  const { data: { Data } } = await axios(url);
+
+  // Validar uno por uno
+  const validCryptos = Data.filter((item: unknown) => {
+    const result = CryptoCurrencyResponseSchema.safeParse(item);
+    return result.success;
+  });
+
+  console.log(`Recibidas: ${Data.length}, Válidas: ${validCryptos.length}`);
+  return validCryptos;
 }
 
-
 export async function fetchCurrentCryptoPrice(pair: Pair) {
-    const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.cryptocurrency}&tsyms=${pair.currency}`
-    const { data: { DISPLAY } } = await axios(url)
-    const result = CryptoPriceSchema.safeParse(DISPLAY[pair.cryptocurrency][pair.currency])
-    if(result.success) {
-        console.log(result.data) 
-        return result.data
-    }
+  const url = `https://min-api.cryptocompare.com/data/pricemultifull?fsyms=${pair.cryptocurrency}&tsyms=${pair.currency}`;
+  const { data: { DISPLAY } } = await axios(url);
+  
+  const result = CryptoPriceSchema.safeParse(DISPLAY[pair.cryptocurrency][pair.currency]);
+  
+  if (result.success) {
+    console.log("Precio:", result.data);
+    return result.data;
+  }
+
+  throw new Error("No se pudo obtener el precio.");
 }
